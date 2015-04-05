8716b5fdeb7e587875e856cc39245e57eb7bff12
Zhihui <xiewisdom@gmail.com>
2015-04-05 15:22:53 -0700

    Revision

--- Project5_Make Effective Data Visualization/js/myChart_final.js
+++ Project5_Make Effective Data Visualization/js/myChart_final.js
@@ -12,7 +12,7 @@ function draw(data) {
           d3.select("#mychart")
             .append("h2")
                .attr("id", "title")
-               .text("Global Trends for Startup Markets: 1990 to 2014");
+               .text("Trends for Startup Industries: 1990 to 2013");
 
           var svg = d3.select("#mychart")
             .append("svg")
@@ -26,23 +26,27 @@ function draw(data) {
       */
 
           var myChart = new dimple.chart(svg, data);
-          var x = myChart.addTimeAxis("x", "founded_year"); 
-          var y = myChart.addMeasureAxis("y", "percentage");
-          //var z = myChart.addMeasureAxis("z", "freq") // try bubble chart
+          var x = myChart.addTimeAxis("x", "Founded year of startups"); 
+          var y = myChart.addMeasureAxis("y", "Percentage of this startup industry (number of startups in this industry normalized with total number of founded startups)");
+          var z = myChart.addMeasureAxis("z", "Number of startups in this industry") // try bubble chart
           x.dateParseFormat = "%Y";
           x.tickFormat = "%Y";
           x.timeInterval = 1;
-          x.title = "Year";
+          x.title = "Founded year of startups";
           x.fontSize = "17px"; //set font size for x label
           y.dateParseFormat = ".1%";
           y.tickFormat = ".1%";
-          y.title = "Percentage of Startup Market Share";
+          y.overrideMax = 0.25;
+          y.title = "Percentage of Startups in Industries";
           y.fontSize = "17px"; //set font size for y label
-          //myChart.addSeries("market", dimple.plot.line); // try line chart
-          //myChart.addSeries("market", dimple.plot.scatter); //try scatter chart
-          myChart.addSeries("market", dimple.plot.area);
-          myChart.addSeries("market", dimple.plot.bar);
-          var legend = myChart.addLegend(width*0.5, 40, width*0.4, 40, 'right');
+          var Series1 = myChart.addSeries("Startup industry", dimple.plot.line); // try line chart
+          var Series2 = myChart.addSeries("Startup industry", dimple.plot.scatter); //try scatter chart
+          //myChart.addSeries("Startup industries", dimple.plot.area);
+          //myChart.addSeries("Startup industries", dimple.plot.bar);
+          // Change order of legends
+          Series1.addOrderRule([" Software ", " Biotechnology ", " Mobile ", " E-Commerce ", " Other (Real Estate, Travel, Fashion, Consulting, Education, Social Media) "])
+          Series1.addOrderRule([" Software ", " Biotechnology ", " Mobile ", " E-Commerce ", " Other (Real Estate, Travel, Fashion, Consulting, Education, Social Media) "])
+          var legend = myChart.addLegend(width*0.9, 10, 100, 100, "right");
           legend.fontSize = "17px"; // set font size for legend
           myChart.draw();
 
@@ -51,20 +55,33 @@ function draw(data) {
           d3.selectAll("line")
              .style("opacity", 0);
 
+          d3.selectAll("circle")
+             .style("opacity", 0)
+             .on("click", function() {
+               d3.select(this)
+                 .style({"stroke-width": "5px"})
+                 .style("opacity", 1);
+               }).on("dblclick", function() {
+               d3.select(this)
+                 .style({"stroke-width": "1px"})
+                 .style("opacity", 0);
+               })
+
         /* 
           Apply mouse click event: single click to show area, double click to hide area
         */
 
           d3.selectAll("path")
-             .style("opacity", 0.1)
+             .style("opacity", 0.2)
+             .style({"stroke-width": "4px"})
              .on("click", function() {
                d3.select(this)
-                 .style({"stroke-width": "3px"})
+                 .style({"stroke-width": "8px"})
                  .style("opacity", 1);
                }).on("dblclick", function() {
                d3.select(this)
-                 .style({"stroke-width": "1px"})
-                 .style("opacity", 0.1);
+                 .style({"stroke-width": "4px"})
+                 .style("opacity", 0.2);
                })
         };
 
@@ -73,4 +90,4 @@ function draw(data) {
     and pass the contents of it to the draw function
   */
 
-  d3.csv("data/data.csv", draw);
+  d3.csv("clean_data_R/data.csv", draw);
